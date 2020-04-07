6d7eb6f7ed8504f8986deb8f9ce36adce54bd262
leocassiosilva <leocassiosilva1234@gmail.com>
2020-04-07 20:39:27 -0300

    Subindo

---
+++ dao/Conexao.php
@@ -0,0 +1,24 @@
+<?php
+//CRIANDO A CLASSE DE CONEXAO
+class Conexao{
+	//ATRIBUTO PRIVADOS
+	private $usuario = "root";
+	private $senha= "";
+	private $banco= "covidSolitario";
+	private $servidor = "localhost";
+	private static $conexao;
+	//CONSTRUTOR
+	public function __construct(){}
+	//METODO PARA CONECTAR
+	public function conectar(){
+		try{
+			if(is_null(self::$conexao)){
+				self::$conexao = new PDO("mysql:host=".$this->servidor.";dbname=".$this->banco, $this->usuario, $this->senha);
+			}
+			return self::$conexao;
+		}catch(PDOException $e){
+			echo 'Error: '.$e->getMessage();
+		}
+	}
+}
+?>
