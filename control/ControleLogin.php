d585ed3b601dccd0d1d5ac301bcac352a9b90bab
leocassiosilva <leocassiosilva1234@gmail.com>
2020-04-09 06:53:04 -0300

    Subindo

---
+++ control/ControleLogin.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once("../model/Usuario.php");
+require_once("../dao/loginDAO.php");
+
+$loginDAO = new loginDAO();
+
+
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+//Criando um usuario 
+$usuario = new Usuario(); 
+
+//Verificar o email
+if (empty($email)) {
+	$retorno = array('codigo' => 0, 'mensagem' => 'Preencha seu e-mail!');
+	echo json_encode($retorno);
+	exit();
+}
+
+//
+if (empty($senha)) {
+	$retorno = array('codigo' => 0, 'mensagem' => 'Preencha sua senha!');
+	echo json_encode($retorno);
+	exit();
+}
+
+/*Usuario*/
+$usuario->setEmail($email);
+$usuario->setSenha($senha);
+
+
+$resultado = $loginDAO->login($usuario); 
+
+/*Serve para verificar se o usuario foi logado ou não.*/
+if(!empty($resultado)){
+	$retorno = array('codigo' => 1, 'mensagem' => 'Logado com sucesso!');
+	echo json_encode($retorno);
+}else {
+	$retorno = array('codigo' => 0, 'mensagem' => 'Email ou Senha errado!');
+	echo json_encode($retorno);
+}
+?>
---
+++ dao/loginDAO.php
@@ -0,0 +1,46 @@
+<?php
+
+include("Conexao.php");
+require_once("../model/Usuario.php");
+
+class loginDAO
+{
+
+  private $conexao;
+
+
+  public function __construct()
+  {
+    $this->conexao = new Conexao();
+  }
+
+
+  public function login(Usuario $usuario)
+  {
+
+    $senha = $usuario->getSenha();
+    $email = $usuario->getEmail();
+
+    $query = $this->conexao->conectar()->prepare("SELECT nome, email, id_tipo, id_usuario FROM usuario WHERE email = :email AND senha = :senha");
+    $query->bindValue(":email", $email);
+    $query->bindValue(":senha", $senha);
+    $query->execute();
+    $resultado = $query->fetch(PDO::FETCH_ASSOC);
+
+
+    if(!empty($resultado)){
+      $_SESSION["nome"] = $resultado["nome"];
+      $_SESSION['celular'] = $resultado["celular"];
+      $_SESSION["email"] = $resultado["email"];
+      $_SESSION["cep"] = $resultado["cep"];
+      $_SESSION["cidade"] = $resultado["cidade"];
+       $_SESSION["uf"] = $resultado["uf"];
+      $_SESSION['id_usuario'] = $resultado["id_usuario"];
+      $_SESSION['logado'] = 'sim';
+      return $resultado;
+    } 
+
+  }
+}
+
+?>
