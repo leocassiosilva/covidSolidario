926f7f7b7ddeb49fba426e4c67f5adff6803963a
leocassiosilva <leocassiosilva1234@gmail.com>
2020-04-23 09:25:32 -0300

    Subindo

--- view/layout/navbar1.php
+++ view/layout/navbar1.php
@@ -1,24 +1,27 @@
 <nav class="navbar  navbar-expand-lg navbar-dark maximo" style="background-color: #5abba7; width: 100%;" >
-    <div class="container">
-      <a class="navbar-brand" href="#">Covid Solidário</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="minhasdoacoes.php">Minhas doações</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="doacoes.php">Doadores</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Nome do usuario:</i> <?=$nome ?> <span class="sr-only">(Página atual)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../control/logoff.php">Sair</a>
-          </li>
-        </ul>
-      </div>
+  <div class="container">
+    <a class="navbar-brand" href="#">Covid Solidário</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="home.php">Pagina Inicial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="minhasdoacoes.php">Minhas doações</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="doacoes.php">Doadores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nome do usuario:</i> <?=$nome ?> <span class="sr-only">(Página atual)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../control/logoff.php">Sair</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
