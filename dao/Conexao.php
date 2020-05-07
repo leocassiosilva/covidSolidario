c167de87a661bd42d8eb04b0a22aafd396658da7
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-07 15:35:23 -0300

    Subindo

--- dao/Conexao.php
+++ dao/Conexao.php
@@ -2,10 +2,10 @@
 //CRIANDO A CLASSE DE CONEXAO
 class Conexao{
 	//ATRIBUTO PRIVADOS
-	private $usuario = "postgres";
-	private $senha= "1234";
-	private $banco= "covidsolidario";
-	private $servidor = "localhost";
+	private $usuario = "nvohxwcldzkfmc";
+	private $senha= "17f1eb57fbc11dbc91463ebc4716d0d9c9702548c0d531161811049a6e5c0342";
+	private $banco= "d8v54a9tbsehej";
+	private $servidor = "ec2-52-202-146-43.compute-1.amazonaws.com";
 	private static $conexao;
     private $porta = 5432;
 
