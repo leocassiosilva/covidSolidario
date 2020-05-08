1a854bf072cdd5ab00df1ff068916eea5cdb1e86
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-08 11:41:59 -0300

    Subindo

--- index1.php
+++ index1.php
@@ -36,45 +36,51 @@
 
     <!-- Page Content -->
     <div class="container">
-
-      <!-- Page Heading -->
-      <h1 class="my-4">Page Heading
-        <small>Secondary Text</small>
-      </h1>
-
       <div class="row">
-        <div class="col-lg-4 col-sm-12 mb-4">
-          <div class="card h-200">
-            <div class="card-body">
-             <div class="card-header">
-              <h4 class="my-0 font-weight-normal">Quem pode participar?</h4>
-            </div>
-            <p class="card-text">Qualquer pessoa que deseja doar ou receber doações. Antes é necessário fazer um cadastro simples no site.</p>
-          </div>
-        </div>
-      </div>
+        <div class="col-lg-4 col-sm-12 mb-12">
+         <h1 class="display-4">O que é o Covid Solidário ?</h1>
+         <p class="lead">É um site que tem como objetivo facilitar a comunicação entre doadores e receptores. </p>
+       </div>
+     </div>
+
+     <!-- Page Heading -->
+     <h1 class="my-4">Page Heading
+      <small>Secondary Text</small>
+    </h1>
+
+    <div class="row">
       <div class="col-lg-4 col-sm-12 mb-4">
         <div class="card h-200">
           <div class="card-body">
-            <div class="card-header">
-              <h4 class="my-0 font-weight-normal">Como me cadastrar?</h4>
-            </div>
-            <p class="card-text">Para realizar seu cadastro você precisa clicar no link do menu acima e preencher os dados do formulário.</p>
+           <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Quem pode participar?</h4>
           </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-sm-12 mb-4">
-        <div class="card h-200">
-          <div class="card-body">
-            <div class="card-header">
-              <h4 class="my-0 font-weight-normal">Como posso doar ?</h4>
-            </div>
-            <p class="card-text">Para doar você precisa estar logado no sistema e descrever a sua doação. </p>
-          </div>
+          <p class="card-text">Qualquer pessoa que deseja doar ou receber doações. Antes é necessário fazer um cadastro simples no site.</p>
         </div>
       </div>
     </div>
-     <?php include 'view/layout/footer.php';?>
+    <div class="col-lg-4 col-sm-12 mb-4">
+      <div class="card h-200">
+        <div class="card-body">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Como me cadastrar?</h4>
+          </div>
+          <p class="card-text">Para realizar seu cadastro você precisa clicar no link do menu acima e preencher os dados do formulário.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-12 mb-4">
+      <div class="card h-200">
+        <div class="card-body">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Como posso doar ?</h4>
+          </div>
+          <p class="card-text">Para doar você precisa estar logado no sistema e descrever a sua doação. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include 'view/layout/footer.php';?>
 
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
