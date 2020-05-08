07f40d9b4db1f816f7306a71d7d14d7afc68f4d1
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-07 21:11:27 -0300

    Subindo

--- index.php
+++ index.php
@@ -41,42 +41,33 @@
     </div>
     <div class="row justify-content-center">
 
-      <div class="card m-4 mb-4 shadow-sm col-4 col-md-8">
-        <div class="card-header">
-          <h4 class="my-0 font-weight-normal">Quem pode participar?</h4>
+      <div class="card-deck">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Quem pode participar?</h5>
+            <p class="card-text">Qualquer pessoa que deseja doar ou receber doações. Antes é necessário fazer um cadastro simples no site.</p>
+          </div>
         </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li><p>Qualquer pessoa que deseja doar ou receber doações. Antes é necessário fazer um cadastro simples no site.</p></li>
-          </ul>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Como me cadastrar?</h5>
+            <p class="card-text">Para realizar seu cadastro você precisa clicar no link do menu acima e preencher os dados do formulário.</p>
+          </div>
         </div>
-      </div>
-      <div class="card m-4 mb-4 shadow-sm col-4 col-md-8">
-        <div class="card-header">
-          <h4 class="my-0 font-weight-normal">Como me cadastrar?</h4>
+         <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Como posso doar ?</h5>
+            <p class="card-text">Para doar você precisa estar logado no sistema e descrever a sua doação.</p>
+          </div>
         </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li><p>Para realizar seu cadastro você precisa clicar no link do menu acima e preencher os dados do formulário.</p></li>
-          </ul>
-        </div>
-      </div>
-      <div class="card m-4 mb-4 shadow-sm col-4 col-md-8">
-        <div class="card-header">
-          <h4 class="my-0 font-weight-normal">Como posso doar ?</h4>
-        </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li><p>Para doar você precisa estar logado no sistema e descrever a sua doação.</p></li>
-          </ul>
-        </div>
+      
       </div>
     </div>
   </div>
-  <?php include 'view/layout/footer.php';?>
+    <?php include 'view/layout/footer.php';?>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-</html>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+  </html>
