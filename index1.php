88c8bac4f533f1decb472ce3d16cd3ce1a699632
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-08 12:04:08 -0300

    Subindo

--- index1.php
+++ index1.php
@@ -2,88 +2,95 @@
 <html lang="pt-br">
 <head>
   <meta http-equiv="Content-Language" content="pt-br">
-  <title>Covid Solidarrio</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Listar doadores</title>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" type="text/css" href="../resources/css/style.css" media="screen" />
 
 </head>
 <body>
-  <div id="main">
-    <nav  id="navbar" class="navbar  navbar-expand-lg navbar-dark maximo" style="background-color: #5abba7;" >
-      <div class="container">
-        <a class="navbar-brand" href="#">Covid Solidário</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="view/listar.php">Doadores</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="view/cadastroUsuario.php">Cadastrar</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="view/login.php">Entrar</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+ <nav class="navbar  navbar-expand-lg navbar-dark maximo" style="background-color: #5abba7;" >
+  <div class="container">
+    <a class="navbar-brand" href="#">Covid Solidário</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="../index.php">Home</a>
+        </li>
+                <li class="nav-item">
+          <a class="nav-link" href="listar.php">Doadores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../view/cadastroUsuario.php">Cadastrar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../view/login.php">Entrar</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-    <!-- Page Content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 col-sm-12 mb-4 text-center">
-         <h1 class="display-4">O que é o Covid Solidário ?</h1>
-         <p class="lead">É um site que tem como objetivo facilitar a comunicação entre doadores e receptores. </p>
-       </div>
-     </div>
+<div class="container" id="geral">
+  <div class="row" id="mensagemAlerta">
+    <div class="col-md-6 mx-auto" id="listar-alert">
+      <span id="listarmensagem"></span>
+    </div>
+  </div>
 
-     <div class="row">
+  <div class="row">
       <div class="col-lg-4 col-sm-12 mb-4">
-        <div class="card mb-4 shadow-sm">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Quem pode participar?</h4>
-          </div>
-          <div class="card-body">
-            <ul class="list-unstyled mt-3 mb-4">
-              <li><p>Qualquer pessoa que deseja doar ou receber doações. Antes é necessário fazer um cadastro simples no site.</p></li>
-            </ul>
-          </div>
+      <div class="card rounded-0" id="login-form">
+        <div class="card-header">
+          <h3 class="mb-0" style="text-align: center;">Pesquisar doadores</h3>
         </div>
-      </div>
-      <div class="col-lg-4 col-sm-12 mb-4">
-        <div class="card mb-4 shadow-sm">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Como me cadastrar?</h4>
+        <div class="card-body">
+          <form method="POST">
+           <div class="form-row">
+             <div class="form-group col-md-4">
+               <label for="inputCep">CEP</label>
+               <input type="text" class="form-control" name="cep" id="cep" required=""
+               placeholder="99999-999">
+             </div>
+             <div class="form-group col-md-5">
+              <label for="inputCidade">Cidade</label>
+              <input type="text" class="form-control" id="cidade" name= "cidade"required="" placeholder="Pau dos Ferros">
+            </div>
+            <div class="form-group col-md-3">
+              <label for="inputUf">UF</label>
+              <input type="text" class="form-control" id="uf" name= "uf"required="" placeholder="RN">
+            </div>
           </div>
-          <div class="card-body">
-            <ul class="list-unstyled mt-3 mb-4">
-              <li><p>Para realizar seu cadastro você precisa clicar no link do menu acima e preencher os dados do formulário.</p></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-sm-12 mb-4">
-        <div class="card mb-4 shadow-sm">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Como posso doar ?</h4>
-          </div>
-          <div class="card-body">
-            <ul class="list-unstyled mt-3 mb-4">
-              <li><p>Para doar você precisa estar logado no sistema e descrever a sua doação. Exemplo: Arroz 1kg.</p></li>
-            </ul>
-          </div>
-        </div>
+          <button type="submit" class="btn btn-success btn-lg " id="btnListar" name="btnListar">Pesquisar</button>
+        </form>
       </div>
     </div>
-    <?php include 'view/layout/footer.php';?>
+  </div>
+</div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-  </html>
+<div class="row" id="tabela">
+  <div class="col-md-12 mx-auto" >
+    <div class="table-responsive">
+      <table class="table" id="idTabela" cellspacing="0" width="100%" role="grid" aria-describedby="example_info" style="width: 100%;table-layout:fixed;">
+
+      </table>
+    </div>
+  </div>
+</div>
+</div>
+
+
+<?php include 'layout/footer.php';?>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../resources/js/jquery.maskedinput-1.3.1.min.js"></script>
+<script type="text/javascript" src="../resources/js/mascara_cep_cel.js"> </script>
+
+<script type="text/javascript" src="../resources/js/listar_doacoes.js"></script>
+</html>
