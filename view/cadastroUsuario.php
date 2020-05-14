e6d1912e9fc0511b87c12bff2147bd95e9db1d73
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-14 17:48:10 -0300

    Subindo

--- view/cadastroUsuario.php
+++ view/cadastroUsuario.php
@@ -6,51 +6,76 @@
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" type="text/css" href="../resources/css/style.css" media="screen" />
 </head>
-
 <body>
+  <?php include 'layout/navbar.php';?>
+
   <div class="container">
     <div class="row">
-      <div class="col-lg-10 col-xl-9 mx-auto">
-        <div class="card card-signin flex-row my-5">
-          <div class="card-img-left d-none d-md-flex">
-             <!-- Background image for card set in CSS! -->
-          </div>
-          <div class="card-body">
-            <h5 class="card-title text-center">Register</h5>
-            <form class="form-signin">
-              <div class="form-label-group">
-                <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
-                <label for="inputUserame">Username</label>
-              </div>
-
-              <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                <label for="inputEmail">Email address</label>
-              </div>
-              
-              <hr>
-
-              <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <label for="inputPassword">Password</label>
-              </div>
-              
-              <div class="form-label-group">
-                <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Password" required>
-                <label for="inputConfirmPassword">Confirm password</label>
-              </div>
-
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Register</button>
-              <a class="d-block text-center mt-2 small" href="#">Sign In</a>
-              <hr class="my-4">
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign up with Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign up with Facebook</button>
-            </form>
-          </div>
-        </div>
+      <div id="cad-alert" style="display: none;">
+        <span id="mensagem"></span>
       </div>
     </div>
-  </div>
+    <div class="row">
+      <div class="col-sm-6 col-xs-5 mx-auto">
+        <div class="card card-signin flex-row my-5">
+         <div class="card-body">
+          <h5 class="card-title text-center">Cadastra-se</h5>
+          <form class="form-signin">
+            <div class="form-label-group col-sm-12 mb-4">
+              <label for="inputNome" id="label">Nome</label>
+              <input type="text" class="form-control" name="nome" id="nome" required="">
+            </div>
+
+            <div class="form-label-group col-sm-12 mb-4">
+              <label for="inputCelular" id="label">Whatsapp</label>
+              <input type="tel" class="form-control" name="celular" id="celular" required="" placeholder="(84)9999-99999">
+            </div>
+
+            <div class="form-label-group col-sm-12 mb-4">
+              <label for="inputEmail" id="label">E-mail</label>
+              <input type="email" class="form-control" name="email" id="email" required="">
+            </div>
+
+            <div class="form-label-group col-sm-12 mb-4">
+             <label for="inputCep" id="label">CEP</label>
+             <input type="text" class="form-control" name="cep" id="cep" required=""
+             placeholder="99999-999">
+           </div>
+           <div class="form-label-group col-sm-12 mb-4">
+            <label for="inputCidade" id="label">Cidade</label>
+            <input type="text" class="form-control" id="cidade" name= "cidade"required="" placeholder="Pau dos Ferros">
+          </div>
+          <div class="form-label-group col-sm-12 mb-4">
+            <label for="inputUf" id="label">UF</label>
+            <input type="text" class="form-control" id="uf" name= "uf"required="" placeholder="RN">
+          </div>
+
+          <div class="form-label-group col-sm-12 mb-4">
+            <div class="form-group">
+              <label id="label">Senha</label>
+              <input type="password" class="form-control" id="senha" name="senha" required="">
+            </div>
+          </div>
+          <div class="form-label-group col-sm-12 mb-4">
+            <div class="form-group">
+              <label id="label">Confirmar Senha</label>
+              <input type="password" class="form-control" id="senha2" name="senha2" required="">
+            </div>
+          </div>
+
+          <hr>
+          <div class="form-label-group col-sm-12">
+
+           <button class="btn btn-success text-uppercase" id="btnCadastrar" name="btnCadastrar">Cadastrar</button>
+         </div>
+       </form>
+     </div>
+   </div>
+ </div>
+</div>
+</div>
+
+<?php include 'layout/footer.php';?>
 </body>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
