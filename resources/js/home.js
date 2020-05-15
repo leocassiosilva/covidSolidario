f3cc9ef17f7452a450f7a62e4158a1072aa5939e
leocassiosilva <leocassiosilva1234@gmail.com>
2020-05-15 10:26:37 -0300

    Subindo

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,16 +1,18 @@
 $('document').ready(function(){
-	var cont = 1;
+      var tam = $(window).width();
+      var cont = 1;
             //https://api.jquery.com/click/
             $('#add-campo').click(function () {
-            	$("#add-campo").blur();
-            	cont++;
+                  $("#add-campo").blur();
+                  cont++;
                 //https://api.jquery.com/append/
                 $('#formulario').append('<div class="form-group" id="campo' + cont + '"><div class="form-row"> <div class="form-group col-md-5" id="classeSecundaria"><label>Nome do Produto: </label><br><input type="text" name="nome[]" placeholder="Nome" id="campo' + cont + '" class="form-control"></div><div class="form-group col-md-3" id="classeTerciaria"><label> Quantidade: </label><input type="text" name="quantidade[]" placeholder="Quantidade" id="quantidade' + cont + '" class="form-control"></div> <div class="form-group col-md-4"><label for="inputQuantidade">Remover item</label><br><button type="button" id="' + cont + '" class="btn-apagar btn btn-danger"> Remover </button></div></div> </div>');
-                
+                if (tam <= 1000){
+                      $( "#classePrincipal").addClass( "form-group col-sm-6" ).removeClass( "form-group" );
+                      $( "#classeSecundaria").addClass( "col-sm-12" ).removeClass( "col-md-5" );
+                      $( "#classeTerciaria").addClass( "col-sm-12" ).removeClass( "col-md-3" );
+                }
           });
-
-
-
             $('form').on('click', '.btn-apagar', function () {
                  var button_id = $(this).attr("id");
                  $('#campo' + button_id + '').remove();
